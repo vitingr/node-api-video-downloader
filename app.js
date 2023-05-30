a9ae6af4fdec368af7b615800715216a3c95e75e
vitingr <vitorgabrielsbo1460@gmail.com>
2023-05-30 00:35:36 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const app = express()
+
+const cors = require("cors")
+app.use(cors())
+app.use(express.json())
+
+const ytdl = require("ytdl-core")
+
+app.get("/", (req, res) => {
+    res.send("Bem-Vindo")
+})
+
+app.get("/ytdownload", async (req, res) => {
+    const { url } = req.query
+
+    res.header("Content-Disposition", 'attachmentt; filename="video.mp4"')
+    return ytdl(url).pipe(res)
+})
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando corretamente na porta ${PORT}`)
+})
