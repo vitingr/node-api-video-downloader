c1975d58f9045aa6f62a5af550deadf3367012af
vitingr <vitorgabrielsbo1460@gmail.com>
2023-05-30 15:54:50 -0300

    teste2

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ app.get("/ytdownload", async (req, res) => {
     const { url } = req.query
 
     res.header("Content-Disposition", 'attachmentt; filename="video.mp4"')
-    res.send("Teste")
     return ytdl(url).pipe(res)
 })
 
